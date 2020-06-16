59ac4c8168c9f7760465917e49fc8040779688f3
pavel <60484496+pavel-code-nice@users.noreply.github.com>
2020-06-16 22:56:21 +0300

    fill landvehicle's forms

--- VehicleForms/LVehicleForms/FMotobike.Designer.cs
+++ VehicleForms/LVehicleForms/FMotobike.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labSideCar = new System.Windows.Forms.Label();
+            this.RButSideCarYes = new System.Windows.Forms.RadioButton();
+            this.RButSideCarNo = new System.Windows.Forms.RadioButton();
+            this.ButSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FEngine
+            // 
+            this.FEngine.Location = new System.Drawing.Point(52, 52);
+            // 
+            // labSideCar
+            // 
+            this.labSideCar.AutoSize = true;
+            this.labSideCar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labSideCar.Location = new System.Drawing.Point(469, 164);
+            this.labSideCar.Name = "labSideCar";
+            this.labSideCar.Size = new System.Drawing.Size(141, 20);
+            this.labSideCar.TabIndex = 12;
+            this.labSideCar.Text = "Наличие коляски";
+            // 
+            // RButSideCarYes
+            // 
+            this.RButSideCarYes.AutoSize = true;
+            this.RButSideCarYes.Location = new System.Drawing.Point(651, 167);
+            this.RButSideCarYes.Name = "RButSideCarYes";
+            this.RButSideCarYes.Size = new System.Drawing.Size(40, 17);
+            this.RButSideCarYes.TabIndex = 13;
+            this.RButSideCarYes.TabStop = true;
+            this.RButSideCarYes.Text = "Да";
+            this.RButSideCarYes.UseVisualStyleBackColor = true;
+            // 
+            // RButSideCarNo
+            // 
+            this.RButSideCarNo.AutoSize = true;
+            this.RButSideCarNo.Location = new System.Drawing.Point(710, 167);
+            this.RButSideCarNo.Name = "RButSideCarNo";
+            this.RButSideCarNo.Size = new System.Drawing.Size(44, 17);
+            this.RButSideCarNo.TabIndex = 14;
+            this.RButSideCarNo.TabStop = true;
+            this.RButSideCarNo.Text = "Нет";
+            this.RButSideCarNo.UseVisualStyleBackColor = true;
+            // 
+            // ButSave
+            // 
+            this.ButSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButSave.Location = new System.Drawing.Point(651, 272);
+            this.ButSave.Name = "ButSave";
+            this.ButSave.Size = new System.Drawing.Size(96, 38);
+            this.ButSave.TabIndex = 15;
+            this.ButSave.Text = "Сохранить";
+            this.ButSave.UseVisualStyleBackColor = true;
+            this.ButSave.Click += new System.EventHandler(this.ButSave_Click);
+            // 
+            // FMotobike
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FMotobike";
+            this.ClientSize = new System.Drawing.Size(800, 341);
+            this.Controls.Add(this.ButSave);
+            this.Controls.Add(this.RButSideCarNo);
+            this.Controls.Add(this.RButSideCarYes);
+            this.Controls.Add(this.labSideCar);
+            this.Name = "FMotobike";
+            this.Text = "Мотоцикл";
+            this.Load += new System.EventHandler(this.FMotobike_Load);
+            this.Controls.SetChildIndex(this.labSideCar, 0);
+            this.Controls.SetChildIndex(this.RButSideCarYes, 0);
+            this.Controls.SetChildIndex(this.RButSideCarNo, 0);
+            this.Controls.SetChildIndex(this.ButSave, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labSideCar;
+        private System.Windows.Forms.RadioButton RButSideCarYes;
+        private System.Windows.Forms.RadioButton RButSideCarNo;
+        private System.Windows.Forms.Button ButSave;
     }
 }
--- VehicleForms/LVehicleForms/FMotobike.cs
+++ VehicleForms/LVehicleForms/FMotobike.cs
@@ -7,14 +7,112 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OOTP.Vehicles.LVehicle;
+using OOTP.VehiclesElement;
+using OOTP.VehiclesElementForm;
+using OOTP.Attributes;
 
 namespace OOTP.VehicleForms.LVehicleForms
 {
-    public partial class FMotobike : Form
+    [WorkingTypesForm((new Type[] { typeof(Motobike) }))]
+    public partial class FMotobike : FLandVehicle
     {
+        private Motobike receivedMotobike = null;
         public FMotobike()
         {
+            InitializeForm();
+        }
+        public void SetReadOnly()
+        {
+            foreach (var TextBox in Controls.OfType<TextBox>())
+            {
+                TextBox.ReadOnly = true;
+            }
+            foreach (var ComboBox in Controls.OfType<ComboBox>())
+            {
+                ComboBox.Enabled = false;
+            }
+            foreach (var RadioButton in Controls.OfType<RadioButton>())
+            {
+                RadioButton.Enabled = false;
+            }
+
+            ButSave.Hide();
+        }
+        public FMotobike(object transport, bool readMode)
+        {
+            InitializeForm();
+            SetMotobike((Motobike)transport);
+            if (readMode == true)
+            {
+                SetReadOnly();
+                FEngine.ReadOnly = true;
+            }
+        }
+        private void InitializeForm()
+        {
             InitializeComponent();
+            RButSideCarNo.Checked = true;
+            FEngine = new FEngine();
+        }
+        public void SetSideCar(SideCar sideCar)
+        {
+            switch (sideCar)
+            {
+                case SideCar.yes: RButSideCarYes.Checked = true; break;
+                case SideCar.no: RButSideCarNo.Checked = true; break;
+            }
+        }
+
+        public SideCar GetSideCar()
+        {
+            if (RButSideCarYes.Checked == true)
+                return SideCar.yes;
+            else
+                return SideCar.no;
+        }
+        private void EditMotobike(Motobike motobike)
+        {
+            motobike.Name = GetName();
+            motobike.MaxSpeed = GetMaxSpeed();
+            motobike.Weight = GetWeight();
+            motobike.Year = GetYear();
+            motobike.NumOfWheels = GetNumOfWheels();
+            motobike.SideCar = GetSideCar();
+            Engine engine = new Engine(GetPower(), GetVolume(), GetTypeFuel());
+            motobike.EngineParams = engine;
+        }
+
+        public void SetMotobike(Motobike motobike)
+        {
+            receivedMotobike = motobike;
+            SetName(motobike.Name);
+            SetMaxSpeed(motobike.MaxSpeed);
+            SetWeight(motobike.Weight);
+            SetYear(motobike.Year);
+            SetNumOfWheels(motobike.NumOfWheels);
+            SetSideCar(motobike.SideCar);
+            SetPower(motobike.EngineParams.Power);
+            SetVolume(motobike.EngineParams.Volume);
+            SetTypeFuel(motobike.EngineParams.TypeFuel);
+        }
+
+        private void FMotobike_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButSave_Click(object sender, EventArgs e)
+        {
+            if (receivedMotobike == null)
+            {
+                Motobike motobike = new Motobike();
+                EditMotobike(motobike);
+                FMenu.AddTransport(motobike);
+            }
+            else
+                EditMotobike(receivedMotobike);
+            Close();
         }
     }
 }
